97bf71057649ae627ca4bb381932e4cdbca00d69
zxcfdsew <seonghi1313@naver.com>
2024-10-28 09:21:46 +0900

    데이터 수정 완료

--- src/main/java/com/meongnyang/shop/controller/admin/AdminProductController.java
+++ src/main/java/com/meongnyang/shop/controller/admin/AdminProductController.java
@@ -52,8 +52,7 @@ public class AdminProductController {
 
     @DeleteMapping("/products")
     public ResponseEntity<?> deleteProducts(ReqDeleteProductDto dto) {
-        System.out.println(dto);
-//        adminProductService.deleteProductById(dto);
+        adminProductService.deleteProductById(dto);
         return ResponseEntity.ok().body(true);
     }
 
