de3ed5ff79d7d083e555350915934c6713fc90d8
hyunju1219 <kk020123@naver.com>
2024-11-01 12:49:41 +0900

    주문해야됨

---
+++ src/main/java/com/meongnyang/shop/dto/response/user/RespCheckProductsDto.java
@@ -0,0 +1,24 @@
+package com.meongnyang.shop.dto.response.user;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class RespCheckProductsDto {
+    private List<CheckProduct> checkProducts;
+    private int productsCount;
+
+    @Data
+    @Builder
+    public static class CheckProduct {
+        private Long productId;
+        private String productName;
+        private Long productPrice;
+        private String groupName;
+        private String categoryName;
+        private String imgName;
+    }
+}
--- src/main/java/com/meongnyang/shop/repository/user/UserProductMapper.java
+++ src/main/java/com/meongnyang/shop/repository/user/UserProductMapper.java
@@ -5,10 +5,12 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Mapper
 public interface UserProductMapper {
     List<Product> findProducts(Map<String, Object> params);
     int findProductCount(Map<String, Object> params);
     Product findProductById(Long id);
+    List<Product> findProductsById(List<Long> ids);
 }
