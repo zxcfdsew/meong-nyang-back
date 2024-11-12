6c265c4fa9961c30758dc0b2b7f7d891e2d72f32
zxcfdsew <seonghi1313@naver.com>
2024-11-12 15:41:22 +0900

    기능 구현 중단

--- src/main/java/com/meongnyang/shop/entity/User.java
+++ src/main/java/com/meongnyang/shop/entity/User.java
@@ -25,6 +25,7 @@ public class User {
     private String phone;
     private int membershipLevelId;
     private LocalDate createDate;
+    private LocalDate recentPurchaseDate;
     private String provider;
 
     private Address address;
